24ce4a9bc2d4b26d148675f4b4b2196959346cd4
adjlevis <adjlevis@qq.com>
2025-11-11 13:24:41 +0800

    Update docker-build.sh

--- immortalwrt/docker-build.sh
+++ immortalwrt/docker-build.sh
@@ -3,8 +3,17 @@ set -euxo pipefail
 
 WORKDIR=$(pwd)
 
+echo "🚀 开始准备构建环境..."
+
 docker run --rm --user root \
   -v "${WORKDIR}":/home/build/immortalwrt \
   -w /home/build/immortalwrt \
   immortalwrt/imagebuilder:armsr-armv7-openwrt-24.10 \
-  bash ./build.sh
+  bash -c "
+    set -euxo pipefail
+    echo '🚀 更新 feeds...'
+    ./scripts/feeds update -a
+    ./scripts/feeds install -a
+    echo '🚀 开始编译固件...'
+    bash ./build.sh
+  "
