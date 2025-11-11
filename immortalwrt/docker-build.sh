b41d1e1751f08d385dbb4d12dcdbf264d279685b
adjlevis <adjlevis@qq.com>
2025-11-11 13:31:42 +0800

    Update docker-build.sh

--- immortalwrt/docker-build.sh
+++ immortalwrt/docker-build.sh
@@ -3,15 +3,8 @@ set -euxo pipefail
 
 WORKDIR=$(pwd)
 
-docker run --rm \
-  --user root \
-  -v "$WORKDIR/bin:/home/build/immortalwrt/bin" \
-  -v "$WORKDIR/files:/home/build/immortalwrt/files" \
-  -v "$WORKDIR/build.sh:/home/build/immortalwrt/build.sh" \
+docker run --rm --user root \
+  -v "${WORKDIR}":/home/build/immortalwrt \
+  -w /home/build/immortalwrt \
   immortalwrt/imagebuilder:armsr-armv7-openwrt-24.10 \
-  /home/build/immortalwrt/build.sh
-
-# ✅ 修复权限 & 调试输出
-sudo chmod -R 777 bin || true
-echo "=== Bin 目录内容 ==="
-ls -R bin || true
+  bash ./build.sh
